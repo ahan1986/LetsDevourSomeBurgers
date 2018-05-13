5c13a28223175df51202b185772711c96b1410de
Handrew <andrew.a.han@gmail.com>
2018-05-12 21:56:54 -0400

    added CORS on express.js to allow cross origin access

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
+// trying to use this so that I can enable cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
